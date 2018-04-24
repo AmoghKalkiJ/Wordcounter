68b4a44f76f11bd933263bdcc56e2b22938b8afd
AmoghKalkiJ <amogh.jayanthi@gmail.com>
2018-04-24 14:19:32 +0530

    gofmt check

--- cmd/word_counter.go
+++ cmd/word_counter.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	
+	"Wordcounter/pkg/counter"
+	"fmt"
 	"github.com/gorilla/mux"
-        "Wordcounter/pkg/counter"
-        "net/http"
-        "fmt"	
+	"net/http"
 )
-
-
 
 func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/counter", counter.Portal)
 	r.HandleFunc("/api/wordcounter", counter.Wordcounter)
-        fmt.Println("Started server on  localhost:8082")
-	if err := http.ListenAndServe(":8082",r); err != nil {
+	fmt.Println("Started server on  localhost:8082")
+	if err := http.ListenAndServe(":8082", r); err != nil {
 		panic(err)
 	}
-        
+
 }
